d66db1f4b5aa3cb330e082a0ce431c253db8ad1a
Riswan Noorhidayat <44827791+riswannh@users.noreply.github.com>
2020-09-06 21:00:43 +0800

    add penerima

---
+++ app/src/main/java/id/pdam/simdam/main/suin/main/suin/compose/SearchPenerimaVM.java
@@ -0,0 +1,7 @@
+package id.pdam.simdam.main.suin.main.suin.compose;
+
+import androidx.lifecycle.ViewModel;
+
+public class SearchPenerimaVM extends ViewModel {
+    // TODO: Implement the ViewModel
+}
